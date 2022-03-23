a46b94e173336b3fad4486a539293a866544a802
Happiness44 <dkfma940507@naver.com>
2022-03-23 23:44:10 +0900

    feat: 3일차 정리

---
+++ 3_day/function.py
@@ -0,0 +1,77 @@
+# 파이썬 함수 실습
+
+# 사칙 연산
+def add(a, b):
+    # 더하기
+    return a + b
+
+
+def subtract(a, b):
+    # 뺴기
+    return a - b
+
+
+def multiply(a, b):
+    # 곱하기
+    return a * b
+
+
+def divide(a, b):
+    # 나누기
+    return a / b
+
+
+a = 3
+b = 5
+print("a + b = ", add(a, b))
+print("a - b = ", subtract(a, b))
+print("a * b = ", multiply(a, b))
+print("a / b = ", divide(a, b))
+
+
+# greeting
+# 매개 변수로 이름을 넣으면 "안녕 {이름}?"이라고 출력하는 greeting 함수를 만들어 봅시다.
+# {이름}에는 매개 변수로 넣은 문자열이 나와야 합니다.
+
+# formatting : 문자열 안에 변수를 넣을 수 있음
+def greeting(name):
+    # 인사를 하는 함수
+    return f"안녕? {name}"
+
+
+name = '아름'
+print(greeting(name))
+print(greeting('다롱이'))
+
+
+# 화씨 -> 섭씨, 섭씨 -> 화씨
+# 화씨를 섭씨로 만드는 공식 : (? - 32) / 1.8
+# 섭씨를 화씨로 만드는 공식 : (? * 1.8) + 32
+def f_to_c(degree):
+    # 화씨를 섭씨로
+    return (degree - 32) / 1.8
+
+
+print("f_to_c : ", f_to_c(32))
+
+
+def c_to_f(degree):
+    # 섭씨를 화씨로
+    return (degree * 1.8) + 32
+
+
+print("c_to_f : ", c_to_f(0))
+
+
+# 3초 후에 인사를 하는 함수
+# 3초라고 고정되어 있는 시간을 매개 변수로 받을 수 있도록 변경
+import time
+
+
+def delay_hello(text, sec):
+    time.sleep(sec)
+    print(text)
+
+
+delay_hello("안녕하세요~ 다롱언니입니다!", 0.8)
+
---
+++ 3_day/homework.py
@@ -0,0 +1,48 @@
+# 어떤 숫자를 넘기면 제곱을 리턴하는 함수 square
+def square(num):
+    return num ** 2
+
+
+print(square(4))
+print(square(5.5))
+
+
+# 밑변과 높이를 넣으면 삼각형의 넓이를 구해주는 함수
+def area(width, height):
+    return width * height / 2
+
+
+print(area(3, 5))
+print(area(4, 8))
+
+
+# 1달러 환율이 1120원이라 가정하고 원을 넣으면 달러로 바꾼 뒤 출력해주는 함수
+def won_to_dollars(won):
+    return round((won / 1120), 2)
+
+
+print(won_to_dollars(100))
+print(won_to_dollars(1120))
+
+
+# 주문 정보(음료 이름, 사이즈, 아이스 여부, 수량)를 넣으면 주문 정보를 출력해주는 함수 order(name, size, is_ice, amount)
+# order은 결과 값으로 받은 정보를 그대로 리턴해야 함
+def order(name, size, is_ice, amount):
+    print(f"[주문정보] 음료 이름 : {name}, 사이즈 : {size}, 아이스 여부 : {is_ice}, 수량 : {amount}")
+    return name, size, is_ice, amount
+
+
+order("아메리카노", "tall", "아이스", 3)
+print(order("딸기라떼", "small", "핫", 1))
+# 리턴을 여러 개 할 경우 -> tuple 타입
+print(type(order("딸기라떼", "small", "핫", 1)))
+
+
+# 주문 정보(음료 이름, 사이즈, 아이스 여부, 수량)를 받아서 커피를 만드는 make_coffee(name, size, is_ice, amount)
+# "주문 하신 {name} {size}사이즈 {is_ice} {amount} 잔 나왔습니다." 가 출력되도록
+def make_coffee(name, size, is_ice, amount):
+    print(f"주문 하신 {name} {size} 사이즈 {is_ice} {amount}잔 나왔습니다.")
+
+
+make_coffee("유자차", "tall", "핫", "2")
+make_coffee("바닐라 라떼", "small", "아이스", "1")
